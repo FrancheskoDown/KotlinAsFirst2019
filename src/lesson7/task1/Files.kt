5998752ea9dfabde236196b9cbba2b11a1e4c7a2
FrancheskoDown <frollite2@gmail.com>
2019-11-30 15:55:46 +0300

    5 and 6 fix, 7

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -440,6 +440,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     stack.push("<p>")
     outputStream.write(stack.peek())
     outputStream.newLine()
+    var pair = false
 
     //Анализ текста
     for (currentLineIndex in text.indices) {
@@ -449,12 +450,24 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
 
         //Расставление абзацев
         if (currentLine.isEmpty()) {
+            outputStream.write("</p>")
+            stack.pop()
+
             var nextLineIndex = currentLineIndex + 1
 
             while (nextLineIndex < textSize && text[nextLineIndex].isEmpty()) nextLineIndex++
-            outputStream.write("</p><p>")
+            if (pair) {
+                if (stack.peek() != "<p>") {
+                    stack.push("<p>")
+                    outputStream.write(stack.peek())
+                } else {
+                    outputStream.write("</p>")
+                    stack.pop()
+                }
+            }
             continue
         }
+        pair = true
 
         //Анализ знаков
         while (currentCharIndex < currentLength) {
