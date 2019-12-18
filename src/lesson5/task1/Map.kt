6f8c42209180d7fd1043de304f177772a3cca519
FrancheskoDown <51823882+FrancheskoDown@users.noreply.github.com>
2019-12-18 21:32:50 +0300

    Last

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -240,8 +240,6 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
 fun canBuildFrom(chars: List<Char>, word: String): Boolean {
-    if (word.isEmpty()) return true
-    if ((word == " " && !chars.contains(' ')) || (chars.isEmpty() && word.isNotEmpty())) return false
     if (word.toLowerCase().toSet().all { it in chars.joinToString("").toLowerCase().toSet() }) return true
     return false
 }
